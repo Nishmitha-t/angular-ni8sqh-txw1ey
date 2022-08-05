a40e67c34892cd32814d93dd2c04b890d1ae108a
Nishmitha-t <nishmithat72434@gmail.com>
2022-08-05 12:49:35 +0530

    1 file modified

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class Logger {
   writeCount(count: number) {
     console.warn(count);
+    alert('You Clicked!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
